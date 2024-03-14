85ab4a3c748b72337a37ef475d1aae0b5b325b1c
B33blebroxx <b33blebroxx@gmail.com>
2024-03-13 20:24:14 -0500

    Changed index to display registration page if user isn't registered.

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,41 @@
 import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import { useEffect, useState } from 'react';
+import { checkUser, signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import RegisterForm from '../components/RegisterForm';
 
 function Home() {
   const { user } = useAuth();
+  const [authUser, setAuthUser] = useState();
+
+  useEffect(() => {
+    checkUser(user.uid).then((data) => setAuthUser(data[0]));
+  }, [user.uid]);
+
+  const onUpdate = () => {
+    checkUser(user.uid).then((data) => setAuthUser(data));
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      {authUser?.uid === user?.uid ? (
+        <div
+          className="text-center d-flex flex-column justify-content-center align-content-center"
+          style={{
+            height: '90vh',
+            padding: '30px',
+            maxWidth: '400px',
+            margin: '0 auto',
+          }}
+        >
+          <h1>Hello {user?.fbUser?.displayName}! </h1>
+          <p>Click the button below to logout!</p>
+          <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+            Sign Out
+          </Button>
+        </div>
+      ) : (<RegisterForm user={user} onUpdate={onUpdate} />)}
+    </>
   );
 }
 
